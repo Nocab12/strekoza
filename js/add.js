ff111ed7a1b1a97d2a0bcdb7380d64b669d34d62
Denis Berezin <nocab139@yandex.ru>
2020-02-20 23:23:02 +0300

    тест модалки 3

--- js/add.js
+++ js/add.js
@@ -71,16 +71,15 @@ $(document).ready(function () {
     });
 
     $(".close-modal").on('click', function(e){
-        e.preventDefault();
-        e.stopImmediatePropagation;
+
 
         var $this = $(this),
             modal = $($this).data("modal");
 
         $(modal).removeClass("open");
-        setTimeout( function(){
+
             $(modal).parents(".overlay").removeClass("open");
-        }, 350);
+
 
     });
 
